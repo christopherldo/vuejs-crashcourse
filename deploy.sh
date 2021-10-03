6606186c51ae924f999975ada9c1e9b1937488dc
Christopher de Olveira <christopherldo@gmail.com>
2021-10-03 13:11:00 -0400

    Correcting git username and repository

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:christopherldo/vuejs-crashcourse.git main:gh-pages
+git push -f git@github.com:christopherldo/vuejs-crashcourse.git master:gh-pages
 cd -
