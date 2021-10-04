c8a1b7a7846e849fed89ddb79ea09bf606717a73
Christopher de Olveira <christopherldo@gmail.com>
2021-10-04 19:29:21 -0400

    deploy

---
+++ js/about.e827f1b8.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,s){"use strict";s.r(e);var n=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"about"},[s("h1",[t._v("About")]),s("p",[t._v(" This is the TodoList app v1.0.0. It is part of the Vue crash course on Youtube ")])])}],o=s("2877"),a={},r=Object(o["a"])(a,n,u,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=about.e827f1b8.js.map
